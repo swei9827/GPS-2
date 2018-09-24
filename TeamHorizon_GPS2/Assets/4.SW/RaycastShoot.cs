bda0dc959869f011904e3386a22d72120caa5d83
Shengwei <sw7177@gmail.com>
2018-09-25 04:48:42 +0800

    Shoot_Revert

--- TeamHorizon_GPS2/Assets/4.SW/RaycastShoot.cs
+++ TeamHorizon_GPS2/Assets/4.SW/RaycastShoot.cs
@@ -1,23 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RaycastShoot : MonoBehaviour {
+public class RayCastShoot : MonoBehaviour {
 
-    public GameObject particle;
-    RaycastHit hit;
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public int gunBullet = 5;
+    public Transform gunEnd;
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
 
+    public Text bulletLeft;
+
+    void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+	
+	// Update is called once per frame
 	void Update () {
+        Debug.Log(Input.acceleration.y);
+        Debug.Log(Input.acceleration.x);
+
+        if (new Vector2(Input.acceleration.x, Input.acceleration.z).magnitude > 2)
+        {
+            gunBullet = 5;
+        }
+        /*if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunEnd.position);
+            if (Physics.Raycast(rayOrigin, fpsCam.ScreenToWorldPoint(Input.mousePosition), out hit, weaponRange))
+            { 
+                laserLine.SetPosition(1, hit.point);
+                ShootableBox health = hit.collider.GetComponent<ShootableBox>();
+                if (health != null)
+                {
+                    health.Damage(gunDamage);
+                }
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }*/
         if (Input.GetMouseButtonDown(0))
         {
-            // Construct a ray from the current touch coordinates
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            // Create a particle if hit
-            if (Physics.Raycast(ray,out hit))
+            Vector3 mousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+            Vector3 mousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+            Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
+            Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunEnd.position);
+            gunBullet--;
+            if (Physics.Raycast(mousePosN,mousePosF-mousePosN,out hit,weaponRange))
             {
-                //Debug.DrawRay(transform.position,(hit.))
-                Instantiate(particle,transform.position,transform.rotation);
-            }                
+                laserLine.SetPosition(1, hit.point);
+            }
         }
+
+        bulletLeft.text = gunBullet.ToString();
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
     }
 }
