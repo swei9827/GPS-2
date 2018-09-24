76966da07dd7c0aec5f8beb807cbce64d0d40884
Shengwei <sw7177@gmail.com>
2018-09-25 01:35:47 +0800

    shoot and reload

---
+++ TeamHorizon_GPS2/Assets/RaycastShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+    public GameObject particle;
+    RaycastHit hit;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Construct a ray from the current touch coordinates
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Create a particle if hit
+            if (Physics.Raycast(ray,out hit))
+            {
+                //Debug.DrawRay(transform.position,(hit.))
+                Instantiate(particle,transform.position,transform.rotation);
+            }
+                
+        }
+    }
+}
