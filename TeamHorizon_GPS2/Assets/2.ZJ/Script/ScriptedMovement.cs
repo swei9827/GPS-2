db240f769ad72dae09bccae6ea5f4bea53191020
zjLee <0118259@kdu-online.com>
2018-10-10 02:57:49 +0800

    Level tutorial, pan to target camera work

---
+++ TeamHorizon_GPS2/Assets/2.ZJ/Script/ScriptedMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedMovement : MonoBehaviour {
+
+    public Transform goal;
+    public List<Transform> locations = new List<Transform>();
+    public float speed;
+    public float turnSpeed;
+    int targetCount = 0;
+
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;               
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            targetCount++;
+        }
+        goal = locations[targetCount];
+
+        Vector3 targetDir = goal.position - transform.position;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, turnSpeed*step, 0.0f);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDir);    
+        transform.position = Vector3.MoveTowards(transform.position, goal.position, step);       
+    }
+}
