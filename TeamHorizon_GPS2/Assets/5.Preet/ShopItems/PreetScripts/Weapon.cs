95fe8c4e5c23326e41fd95557967017b3dfea8a9
PreetProot <41739104+PreetProot@users.noreply.github.com>
2018-10-13 00:06:05 +0800

    QTE trigger/ draw collider/ shop

---
+++ TeamHorizon_GPS2/Assets/5.Preet/ShopItems/PreetScripts/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Weapon
+{
+    public string weaponName;
+    public int weaponID;
+
+    public Sprite unboughtSprite;
+    public Sprite boughtSprite;
+
+    public float weaponPrice;
+    public bool bought;
+
+}
