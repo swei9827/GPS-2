bf615c7dd68effd33ec38e865ba1f639a70a84fc
Lenovo-PC\Lenovo <mengjoo0123@gmail.com>
2018-10-12 22:27:39 +0800

    hp shield

--- TeamHorizon_GPS2/Assets/1.MJ/Scripts/PlayerHp.cs
+++ TeamHorizon_GPS2/Assets/1.MJ/Scripts/PlayerHp.cs
@@ -6,25 +6,43 @@ using UnityEngine.UI;
 public class PlayerHp : MonoBehaviour {
 
 
-    public float startHealth = 100;
+    public float startHealth = 100f;
+    public float startShield = 100f;
+    public bool activeShield = false;
     private float health;
+    private float shield;
 
     public Image healthBar;
-
+    public Image shieldBar;
     // Use this for initialization
     void Start () {
         //healthBar = GetComponent<Image>();
         health = startHealth;
+        shield = startShield;
 	}
 	
 	// Update is called once per frame
 	void TakeDamage (float damage) {
 
-        health -= damage;
-        healthBar.fillAmount = health / startHealth;
-
-        if (health < 0)
+        if(activeShield == false)
         {
+            health -= 50f;
+            healthBar.fillAmount = health / startHealth;
+            Debug.Log("hehehehe");
+
+        }
+
+        else if(activeShield == true)
+        {
+            shield -= 50f;
+            shieldBar.fillAmount = shield / startShield;
+        }
+
+        
+
+        if (healthBar.fillAmount <= 0.5f)
+        {
+            Debug.Log("hehehehe");
             //Die();
             //deathAudio.Play();
 
