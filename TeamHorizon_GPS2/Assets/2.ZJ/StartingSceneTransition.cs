1cdca169d60716af3c6b0f0feaea6efdf7189481
zjLee <0118259@kdu-online.com>
2018-10-16 06:58:28 +0800

    GG logo scene

---
+++ TeamHorizon_GPS2/Assets/2.ZJ/StartingSceneTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartingSceneTransition : MonoBehaviour {
+
+    private IEnumerator coroutine;
+    public float duration;
+    public int index;
+
+    void Start()
+    {
+        coroutine = SceneTransition(duration, index);
+        StartCoroutine(coroutine);
+    }
+
+    public void LoadScene(float waitTime, int sceneIndex)
+    {
+        coroutine = SceneTransition(waitTime, sceneIndex);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator SceneTransition(float waitTime, int sceneIndex)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
