067865fc97f3e1f659e0a637b365030836e61f4d
zjLee <0118259@kdu-online.com>
2018-10-16 19:30:22 +0800

    Added new scene - Scaled Tutorial. Modified fps movement script to input speed parameter

--- TeamHorizon_GPS2/Assets/2.ZJ/Script/PlayerMovement/DemoUseFPSMovement.cs
+++ TeamHorizon_GPS2/Assets/2.ZJ/Script/PlayerMovement/DemoUseFPSMovement.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DemoUseFPSMovement : MonoBehaviour {
-
+    public float speed;
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 100.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
