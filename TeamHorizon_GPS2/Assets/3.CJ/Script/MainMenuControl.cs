414ad937ece40cd179c56b9f979bb98eb8ddcf43
Yoona_Fan <cjng1121@gmail.com>
2018-10-22 17:39:36 +0800

    MainMenu and in game PauseMenu

---
+++ TeamHorizon_GPS2/Assets/3.CJ/Script/MainMenuControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour {
+
+    public GameObject MenuPage;
+    public GameObject SettingPage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void setting()
+    {
+        MenuPage.SetActive(false);
+        SettingPage.SetActive(true);
+    }
+
+    public void backMenuFromSetting()
+    {
+        SettingPage.SetActive(false);
+        MenuPage.SetActive(true);
+    }
+}
