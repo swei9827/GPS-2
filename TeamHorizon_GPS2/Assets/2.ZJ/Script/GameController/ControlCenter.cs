a42123a86af3f2838442aec3cb694bb46b218caa
zjLee <0118259@kdu-online.com>
2018-10-27 23:46:49 +0800

    Updated tutorial level

--- TeamHorizon_GPS2/Assets/2.ZJ/Script/GameController/ControlCenter.cs
+++ TeamHorizon_GPS2/Assets/2.ZJ/Script/GameController/ControlCenter.cs
@@ -279,7 +279,7 @@ public class ControlCenter : MonoBehaviour
                 sMove.PlayerMove(locations[9]);     
                 if(player.transform.position == locations[9].position)
                 {
-                    status = STATUS.INTERACTABLE;
+                    status = STATUS.TURNING;
                     levelStatus = 10;
                 }
             }
@@ -287,6 +287,15 @@ public class ControlCenter : MonoBehaviour
         // Interactable Object
         else if(levelStatus == 10)
         {
+            if(status == STATUS.TURNING)
+            {
+                sMove.PlayerRotate(locations[10]);
+                if((player.transform.eulerAngles.y > 89 && player.transform.eulerAngles.y < 90.05f))
+                {
+                    status = STATUS.INTERACTABLE;
+                }
+            }
+
             if(status == STATUS.INTERACTABLE)
             {
                 if (InteractSuccess)
