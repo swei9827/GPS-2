a301d77b36ab5b882f4e4ae1d40ec42d7df04321
zjLee <0118259@kdu-online.com>
2018-10-30 04:14:40 +0800

    Tree blocking slow player speed

--- TeamHorizon_GPS2/Assets/2.ZJ/Script/Hazard/TreeBlockHazard.cs
+++ TeamHorizon_GPS2/Assets/2.ZJ/Script/Hazard/TreeBlockHazard.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class TreeBlockHazard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private IEnumerator coroutine;
+    public float slowDuration;
+    public float slowSpeed;
+    float originalSpeed = 10.0f;
+
+    void OnTriggerEnter (Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<ScriptedMovement>().speed = slowSpeed;
+            coroutine = ResetSpeed(slowDuration);
+            StartCoroutine(coroutine);
+        }
+    }
+
+    private IEnumerator ResetSpeed(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<ScriptedMovement>().speed = originalSpeed;
+    }
 }
