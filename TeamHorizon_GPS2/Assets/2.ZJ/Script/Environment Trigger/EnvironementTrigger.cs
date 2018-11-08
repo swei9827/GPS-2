1414e4f42fa6037847c9dc0b7db30aa70856bf46
zjLee <0118259@kdu-online.com>
2018-11-08 11:44:54 +0800

    Added destructable/ interactable object, obstacles now can be destroyed once its health = 0

---
+++ TeamHorizon_GPS2/Assets/2.ZJ/Script/Environment Trigger/EnvironementTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironementTrigger : MonoBehaviour {
+
+    public bool Tree;
+    public int ETHealth;
+    float fall = 90;
+
+    void Update()
+    {
+        if (Tree)
+        {
+            if(ETHealth <= 0)
+            {
+                TreeFalling();
+            }
+        }
+    }
+
+    void TreeFalling()
+    {
+        if (fall > 80 && fall <= 90)
+        {
+            fall -= Time.deltaTime * 10;
+        }
+        else if (fall > 70 && fall < 80)
+        {
+            fall -= Time.deltaTime * 15;
+        }
+        else if (fall > 60 && fall < 70)
+        {
+            fall -= Time.deltaTime * 20;
+        }
+        else if (fall > 45 && fall < 60)
+        {
+            fall -= Time.deltaTime * 30;
+        }
+        else if (fall > 25 && fall < 45)
+        {
+            fall -= Time.deltaTime * 40;
+        }
+        else if (fall > 15 && fall < 25)
+        {
+            fall -= Time.deltaTime * 40;
+        }
+
+        transform.rotation = Quaternion.Euler(-Mathf.Clamp(fall, 5, 90), transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+}
---
+++ TeamHorizon_GPS2/Assets/2.ZJ/Script/Obstacles/Obstacles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+    public int ObsHealth;
+
+	void Update () {
+	    if(ObsHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
