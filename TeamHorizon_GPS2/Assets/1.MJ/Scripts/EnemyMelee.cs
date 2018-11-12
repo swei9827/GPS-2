56ca65b7bb32eeb530462ae3d7a8d40e78c19abc
DESKTOP-FQJ2VT8\mengj <mengjoo0123@gmail.com>
2018-11-12 13:24:37 +0800

    Enemy melee + throw projectile

---
+++ TeamHorizon_GPS2/Assets/1.MJ/Scripts/EnemyMelee.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMelee : MonoBehaviour {
+
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    int MinDist = 5;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //enemy melee attack
+            }
+
+        }
+    }
+
+}
---
+++ TeamHorizon_GPS2/Assets/1.MJ/Scripts/EnemyThrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyThrow : MonoBehaviour {
+
+    GameObject prefab;
+    public Transform Player;
+    int MinDist = 10;
+    int MoveSpeed = 4;
+
+
+	void Start () {
+        prefab = Resources.Load("Projectile") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position += transform.forward * MoveSpeed * Time.deltaTime; ;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * 40;
+        }
+	}
+}
