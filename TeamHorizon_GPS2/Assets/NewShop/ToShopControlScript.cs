61fec8180cb15e7dcbb52397849c073ebce8384a
PreetProot <41739104+PreetProot@users.noreply.github.com>
2018-11-17 21:53:39 +0800

    newShop

---
+++ TeamHorizon_GPS2/Assets/NewShop/ToShopControlScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToShopControlScript : MonoBehaviour
+{
+    public Text currencyText;
+    public static int currencyAmount;
+    int isRPGSold;
+    public GameObject RPG;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currencyAmount = PlayerPrefs.GetInt("CurrencyAmount");
+        isRPGSold = PlayerPrefs.GetInt("IsRPGSold");
+
+        if (isRPGSold == 1)
+            RPG.SetActive(true);
+        else
+            RPG.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currencyText.text = "Currency : " + currencyAmount.ToString() + "$";
+	}
+
+    public void goToShop()
+    {
+        PlayerPrefs.SetInt("CurrencyAmount", currencyAmount);
+        SceneManager.LoadScene("NewShop");
+    }
+}
