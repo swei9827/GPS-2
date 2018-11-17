daed2eeb6045bd4b3878e55d125d2518eca5593d
PreetProot <41739104+PreetProot@users.noreply.github.com>
2018-11-18 00:54:26 +0800

    shop

---
+++ TeamHorizon_GPS2/Assets/NewShop/TapforDesc.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapforDesc : MonoBehaviour
+{
+    public Text textShowed = null;
+    public void ChangeWord (string word)
+    {
+        textShowed.text = word;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
